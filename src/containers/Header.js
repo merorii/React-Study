7307c6e40c8fc20fc46566110b9c702a66f3c67c
soaelee <soae0923@gmail.com>
2021-02-02 10:21:10 +0900

    Add: 로그인 팝업창

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import CategoryList from './CategoryList';
 import VideoList from './VideoList';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import Button from '../components/Button';
+import Modal from '../components/Modal';
 
 const HeaderBlock = styled.header`
     position: fixed;
@@ -21,11 +23,21 @@ const NavBlock = styled.nav`
 `;
 
 const Navi = ()=>{
+    const [isLogin, setIsLogin] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
+
+    const onPopup = () =>  setIsVisible(!isVisible);
+
+    const offPopup = () => setIsVisible(false);
     return(
         <HeaderBlock>
             <NavBlock>
                 <CategoryList></CategoryList>
-                <FontAwesomeIcon icon={faBars} />
+                <div>
+                    <Button color="transparent" text={isLogin ? "로그아웃" : "로그인"} onClick={onPopup}/>
+                    <FontAwesomeIcon icon={faBars} />
+                </div>
+                {isVisible && <Modal onClick={offPopup}/>}
             </NavBlock>
             <VideoList/>
         </HeaderBlock>
