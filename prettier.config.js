4109072675e6ea53a8d4003df4fb61b6fd8454e9
eunJeong <poilchxn@imform.co.kr>
2021-02-05 10:25:00 +0900

    프리티어 추가

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true, //'' 실글쿼터 사용
+  jsxSingleQuote: false, // JSX에서는 싱글쿼터 사용 안함 ( " " ) <-회사 컨벤션
+  trailingComma: 'es5', //여러 줄로 된 경우 가능한 한 후행 쉼표를 인쇄
+  tabWidth: 2, //텝누르면 2칸씩 이동
+  semi: true, // 모든 끝나는 문에 ;
+  printWidth: 120, //한줄에 120
+};
