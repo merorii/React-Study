04cdf214de6937cfd40f2c34cec63aa74ea47512
soaelee <soae0923@gmail.com>
2021-02-05 15:56:49 +0900

    Add: animation, Edit: Font-family

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
-
+import Animation from './Animation';
 const Wrapper = styled.div`
   position: fixed;
   top: 0;
@@ -9,7 +9,6 @@ const Wrapper = styled.div`
   right: 0;
   bottom: 0;
   background-image: linear-gradient(to top, #a8edea 0%, #fed6e3 100%);
-
   form {
     position: absolute;
     top: 0;
@@ -32,12 +31,14 @@ const Wrapper = styled.div`
   form input {
     padding: 0.8em 0.5em;
     margin-bottom: 1em;
+    font-family: 'Dancing Script', cursive;
     border: 0;
+    border-bottom: 1px solid white;
     outline: 0;
-    border-radius: 4px;
+    /* border-radius: 4px; */
     font-size: 1rem;
     &:focus {
-      border: 1px solid pink;
+      outline: 1px solid pink;
     }
     background-color: transparent !important;
     :-internal-autofill-selected {
@@ -45,11 +46,15 @@ const Wrapper = styled.div`
     }
   }
   form button {
-    background-color: pink;
+    background-color: transparent;
     border: 0;
     outline: 0;
     width: 100px;
     padding: 0.8em;
+    font-family: 'Dancing Script', cursive;
+    font-size: 2rem;
+    color: white;
+    cursor: pointer;
   }
 `;
 function Login() {
@@ -58,12 +63,14 @@ function Login() {
   return (
     <Wrapper>
       <form onSubmit={handleSubmit(handlerSubmit)}>
-        <input name="nickname" ref={register({ required: true })} />
+        <input name="nickname" ref={register({ required: true })} placeholder="your id"/>
         {errors.nickname && <span>아이디를 입력해주세요.</span>}
-        <input type="password" name="password" ref={register({ required: true })} />
+        <input type="password" name="password" ref={register({ required: true })} placeholder="password"/>
         {errors.password && <span>비밀번호를 입력해주세요.</span>}
-        <button type="submit">로그인</button>
+        <button type="submit">Login</button>
       </form>
+
+      <Animation/>
     </Wrapper>
   );
 }
