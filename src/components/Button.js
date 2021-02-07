ae4d7ed4539075806278e7e96dafc3d7ac547498
eunjeong <poil_chxn@naver.com>
2021-02-07 22:14:34 +0900

    유튜브 스타일 적용

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Children } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -15,15 +15,28 @@ const Wrapper = styled.div`
         opacity:0.8
     }
 `;
+// const Wrapper = styled.div`
+//     background: ${props => props.color === undefined ? '#c4302b' : props.color};
+//     color:${props => props.color === undefined ? '#fff' : 'black'};
+//     display: inline-block;
+//     padding: 0.5rem 1rem;
+//     border-radius:0.5rem;
+//     cursor: pointer;
+//     & + & {
+//     margin-left: 1rem;
+//     }    
+//     &:hover{
+//         opacity:0.8
+//     }
+// `;
 
-const Button = ({text, color, onClick})=>{
-    console.log(color);
+const Button = ({ text, color, onClick, children }) => {
     const handlerClick = () => {
         onClick();
     }
-    return(
+    return (
         <Wrapper color={color} onClick={handlerClick}>
-            {text}
+            { children}
         </Wrapper>
     )
 }
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,41 +1,66 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import CategoryList from './CategoryList';
+import SideMenu from './SideMenu';
 import VideoList from './VideoList';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faBookmark } from '@fortawesome/free-solid-svg-icons';
 import Button from '../components/Button';
 
 import Login from '../components/Login';
 
 const HeaderBlock = styled.header`
   position: fixed;
+  z-index: 100;
   width: 100%;
   box-sizing: border-box;
 `;
 
 const NavBlock = styled.nav`
-  height: 5rem;
   display: flex;
   justify-content: space-between;
+  height: 5rem;
+  width: 100%;
   align-items: center;
-  padding: 0 1rem;
+`;
+
+
+// const ButtonBlock = styled.section`
+//   width: 80vw;
+//   margin: 1rem auto;
+// `;
+const MenuBlock = styled.div`
+  position: relative;
+  left:1rem;
+  z-index: 1;
+  cursor : pointer;
+`;
+const ButtonBlock = styled.div`
+position: relative;
+right: 1rem;
+z-index: 1;
+cursor : pointer;
 `;
 
 const Navi = () => {
   const [isLogin, setIsLogin] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
 
+  const activeSideMenu = () => {
+    isVisible ? setIsVisible(false) : setIsVisible(true);
+  }
   return (
     <HeaderBlock>
       <NavBlock>
-        <CategoryList></CategoryList>
-        <div>
-          <Button color="transparent" text={isLogin ? '로그아웃' : '로그인'} />
-          <FontAwesomeIcon icon={faBars} />
-        </div>
+        <MenuBlock>
+          {/* <Button color="transparent" text={isLogin ? '로그아웃' : '로그인'} /> */}
+          <FontAwesomeIcon icon={faBars} onClick={activeSideMenu} />
+        </MenuBlock>
+        <ButtonBlock>
+          <Button> <FontAwesomeIcon icon={faBookmark} /></Button>
+        </ButtonBlock>
       </NavBlock>
-      {/* <VideoList/> */}
+      {isVisible && <SideMenu />}
     </HeaderBlock>
   );
 };
